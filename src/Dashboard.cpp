effa835f1f78ea52c9ee1536995264dccfbd8b5c
stephb9959 <stephane.bourque@gmail.com>
2022-05-09 10:01:11 -0700

    Framework Update.

--- src/Dashboard.cpp
+++ src/Dashboard.cpp
@@ -11,7 +11,7 @@
 
 namespace OpenWifi {
 	void AnalyticsDashboard::Create() {
-		uint64_t Now = std::time(nullptr);
+		uint64_t Now = OpenWifi::Now();
 		if(LastRun_==0 || (Now-LastRun_)>120) {
 			DB_.reset();
 			//  Todo: call dashboard creation code.
