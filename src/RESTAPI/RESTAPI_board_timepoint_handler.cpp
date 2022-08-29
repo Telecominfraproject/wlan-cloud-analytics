4bb5ee0b86333cd1c2e4c76eb650340c12453b53
stephb9959 <stephane.bourque@gmail.com>
2022-08-29 13:42:34 -0700

    https://telecominfraproject.atlassian.net/browse/WIFI-10547
    Signed-off-by: stephb9959 <stephane.bourque@gmail.com>

--- src/RESTAPI/RESTAPI_board_timepoint_handler.cpp
+++ src/RESTAPI/RESTAPI_board_timepoint_handler.cpp
@@ -30,7 +30,9 @@ namespace OpenWifi {
         auto min_timestamp = std::min_element(points.begin(),points.end(),min_point_timestamp);
         auto max_timestamp = std::max_element(points.begin(),points.end(),min_point_timestamp);
 
-        std::cout << "Max-buckets: " << max_buckets->second << " min timestamp:" << min_timestamp->timestamp << " max timestamp:" << max_timestamp->timestamp << " interval: " << ((max_timestamp-min_timestamp) / max_buckets->second) << std::endl;
+        std::cout << "Max-buckets: " << max_buckets->second << " min timestamp:" << min_timestamp->timestamp <<
+        " max timestamp:" << max_timestamp->timestamp << " interval: " <<
+        ((max_timestamp->timestamp-min_timestamp->timestamp) / max_buckets->second) << std::endl;
 
 
         return max_buckets->second;
