2ec290079fcff4811cfff8cfd82f3a373a33d88e
stephb9959 <stephane.bourque@gmail.com>
2023-02-27 22:35:56 -0800

    https://telecominfraproject.atlassian.net/browse/WIFI-12068
    Signed-off-by: stephb9959 <stephane.bourque@gmail.com>

--- src/RESTAPI/RESTAPI_routers.cpp
+++ src/RESTAPI/RESTAPI_routers.cpp
@@ -7,15 +7,17 @@
 #include "RESTAPI/RESTAPI_board_list_handler.h"
 #include "RESTAPI/RESTAPI_board_timepoint_handler.h"
 #include "RESTAPI/RESTAPI_wificlienthistory_handler.h"
+
 #include "framework/RESTAPI_SystemCommand.h"
 #include "framework/RESTAPI_WebSocketServer.h"
+#include "framework/RESTAPI_SystemConfiguration.h"
 
 namespace OpenWifi {
 
 	Poco::Net::HTTPRequestHandler *
 	RESTAPI_ExtRouter(const std::string &Path, RESTAPIHandler::BindingMap &Bindings,
 					  Poco::Logger &L, RESTAPI_GenericServerAccounting &S, uint64_t TransactionId) {
-		return RESTAPI_Router<RESTAPI_system_command, RESTAPI_board_devices_handler,
+		return RESTAPI_Router<RESTAPI_system_command, RESTAPI_system_configuration, RESTAPI_board_devices_handler,
 							  RESTAPI_board_timepoint_handler, RESTAPI_board_handler,
 							  RESTAPI_board_list_handler, RESTAPI_wificlienthistory_handler,
 							  RESTAPI_webSocketServer>(Path, Bindings, L, S, TransactionId);
@@ -24,7 +26,7 @@ namespace OpenWifi {
 	Poco::Net::HTTPRequestHandler *
 	RESTAPI_IntRouter(const std::string &Path, RESTAPIHandler::BindingMap &Bindings,
 					  Poco::Logger &L, RESTAPI_GenericServerAccounting &S, uint64_t TransactionId) {
-		return RESTAPI_Router_I<RESTAPI_system_command, RESTAPI_board_devices_handler,
+		return RESTAPI_Router_I<RESTAPI_system_command, RESTAPI_system_configuration, RESTAPI_board_devices_handler,
 								RESTAPI_board_timepoint_handler, RESTAPI_board_handler,
 								RESTAPI_board_list_handler, RESTAPI_wificlienthistory_handler>(
 			Path, Bindings, L, S, TransactionId);
